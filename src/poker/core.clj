facdb3c17a6b04b746bd4c4baf1c593ffd9a8fe2
arafeek <adam_rafeek@hotmail.com>
2018-09-05 07:56:10 -0400

    Adds method for converting a string to card and vice versa

--- src/poker/core.clj
+++ src/poker/core.clj
@@ -19,6 +19,25 @@
 (def value-strs ["2" "3" "4" "5" "6" "7" "8" "9" "T" "J" "Q" "K" "A"])
 (def suit-str ["c" "d" "h" "s"])
 
+(def suit-chars {\c 2r1000
+                \d 2r0100
+                \h 2r0010
+                \s 2r0001})
+
+(def rank-chars {\2 0
+                 \3 1
+                 \4 2
+                 \5 3
+                 \6 4
+                 \7 5
+                 \8 6
+                 \9 7
+                 \t 8
+                 \j 9
+                 \q 10
+                 \k 11
+                 \a 12})
+
 ;; a deck is a list of 52 cards
 ;; A card is an int of the form
 ;; xxxbbbbb|bbbbbbbb|cdhsrrrr|xxpppppp
@@ -26,23 +45,18 @@
 ;; cdhs - suit bits, one for each suit
 ;; r - card rank encoding
 ;; p - prime rank encoding
+(defn encode-card
+  "Takes 2 numbers representing a rank and a suit and returns a card encoding"
+  [r s]
+  (bit-or
+    (primes r)
+    (bit-shift-left r 8)
+    (bit-shift-left s 12)
+    (bit-shift-left 1 (+ 16 r))))
+
 (defn make-deck []
   (for [s suits r ranks]
-    (bit-or
-      (primes r)
-      (bit-shift-left r 8)
-      (bit-shift-left s 12)
-      (bit-shift-left 1 (+ 16 r)))))
-
-(defn get-suit
-  "Returns a string representing the value of the suit bits of a card int"
-  [c]
-  (cond
-      (not (zero? (bit-and c 32768))) "c" ;; 15th bit
-      (not (zero? (bit-and c 16384))) "d" ;; 14th bit
-      (not (zero? (bit-and c 8192))) "h"  ;; 13th bit
-      (not (zero? (bit-and c 4096))) "s"  ;; 12th bit
-      :else "--NIL--")) ;; litmus test for an error in our encoding
+    (encode-card r s)))
 
 ;; =======================================================================================
 ;;
@@ -92,15 +106,27 @@
 ;;
 ;; =======================================================================================
 
-(defn parse-card-int
+(defn get-suit
+  "Returns a string representing the value of the suit bits of a card int"
+  [c]
+  (cond
+      (not (zero? (bit-and c 32768))) "c" ;; 15th bit
+      (not (zero? (bit-and c 16384))) "d" ;; 14th bit
+      (not (zero? (bit-and c 8192))) "h"  ;; 13th bit
+      (not (zero? (bit-and c 4096))) "s"  ;; 12th bit
+      :else "--NIL--")) ;; litmus test for an error in our encoding
+
+(defn card->str
   "Returns the string representation of a card int"
   [c]
   (let [r (bit-and (bit-shift-right c 8) 2r1111)
         s (get-suit c)]
     (str (value-strs r) s)))
 
-;; TODO
-(defn parse-card-str
-  "Returns the int represenation of a card string"
+(defn card
+  "Returns the int represenation of a card string. cs has the form <value><suit> eg Ac 2d etc."
   [cs]
-)
+  (let [lower (clojure.string/lower-case cs)
+        v (first lower)
+        s (second lower)]
+    (encode-card (rank-chars v) (suit-chars s))))
